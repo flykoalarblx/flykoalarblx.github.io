eea68a1713e6eeaac9fc4bc3334086074a97545b
abhipin <abhinavpin@gmail.com>
2021-10-25 20:59:40 +0530

    Modified js file

--- controller.js
+++ controller.js
@@ -1,10 +1,10 @@
-var currentPageName = window.location.pathname.split("/").pop();
-console.log(currentPageName)
-if (currentPageName == "index.html") {
+
+var applyButton = document.getElementById("applyButton")
+if (applyButton) {
     document.getElementById("applyButton").addEventListener("click", function() {
         document.location = "apply.html"
     })
-} else if (currentPageName == "apply.html") {
+} else {
     document.getElementById("gcapp").addEventListener("click", function() {
         document.location = "https://docs.google.com/forms/d/e/1FAIpQLSeaZ44gbIZObv9ggwBzCQK1sA6nysUUJGNf3QzWu_wsiQlPdQ/viewform?usp=pp_url"
     })
