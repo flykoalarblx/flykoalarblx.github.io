8df5616f00beea1c6b913751bdb56d798a7df80d
abhipin <abhinavpin@gmail.com>
2021-10-26 14:07:28 +0530

    New content

--- controller.js
+++ controller.js
@@ -9,4 +9,7 @@ if (applyButton) {
     document.getElementById("gcapp").addEventListener("click", function() {
         document.location = "https://docs.google.com/forms/d/e/1FAIpQLSeaZ44gbIZObv9ggwBzCQK1sA6nysUUJGNf3QzWu_wsiQlPdQ/viewform?usp=pp_url"
     })
+    document.getElementById("ccapp").addEventListener("click", function() {
+        document.location = "https://docs.google.com/forms/d/e/1FAIpQLScmo-1BM7c6umJRMaOC9yTrkmbjx2IIadsl6ZrECnLILuOcWg/viewform?usp=pp_url"
+    })
 }
