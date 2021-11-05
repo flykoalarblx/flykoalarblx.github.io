d02f57faf99a70805d0d8e0f6316187c14aaec79
abhipin <abhinavpin@gmail.com>
2021-11-05 18:14:15 +0530

    Updated applications

--- controller.js
+++ controller.js
@@ -1,4 +1,4 @@
-// Version 0.0.1
+// Version 0.0.4
 
 var applyButton = document.getElementById("applyButton")
 if (applyButton) {
@@ -6,10 +6,9 @@ if (applyButton) {
         document.location = "apply.html"
     })
 } else {
-    document.getElementById("gcapp").addEventListener("click", function() {
-        document.location = "https://docs.google.com/forms/d/e/1FAIpQLSeaZ44gbIZObv9ggwBzCQK1sA6nysUUJGNf3QzWu_wsiQlPdQ/viewform?usp=pp_url"
-    })
+    
     document.getElementById("ccapp").addEventListener("click", function() {
         document.location = "https://docs.google.com/forms/d/e/1FAIpQLScmo-1BM7c6umJRMaOC9yTrkmbjx2IIadsl6ZrECnLILuOcWg/viewform?usp=pp_url"
     })
+    
 }
